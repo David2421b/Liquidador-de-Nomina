0b76803f99321e920eeb5fe08a187f2d041b2322
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-17 01:10:03 -0500

    se deja iniciado el metodo para registrar un usuario

--- src/model/clase_empleado.py
+++ src/model/clase_empleado.py
@@ -2,4 +2,4 @@ class Empleado:
     def __init__(self, cedula: str, nombres:str , apellidos: str):
         self.cedula: str = cedula
         self.nombres: str = nombres
-        self.apellidos:str = apellidos 
+        self.apellidos:str = apellidos
--- src/view/web/app.py
+++ src/view/web/app.py
@@ -1,8 +1,31 @@
+import sys
+
+sys.path.append(".")
+
 from flask import Flask
 from flask import render_template, request
 
+from src.controller.nomina_controller import NominaController
+from model.calculo_nomina import Nomina
+from model.clase_empleado import Empleado
+
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/pagina_registro")
+def pagina_registrar():
+    return render_template("registrar.html")
+
+@app.route("/acceder", methods = ["GET", "POST"])
+def acceder():
+    if request.method == "POST":
+        cedula = request.form["cedula"]
+        
+        nomina = Nomina()
 
 
 if __name__ == "__main__":

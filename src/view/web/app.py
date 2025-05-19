77230dc54b8100548c1ae16b2e092181c0a8814e
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-18 20:33:44 -0500

    se corrige el error al mostrar la alerta y se implementa los tipos de variables

--- src/view/web/app.py
+++ src/view/web/app.py
@@ -27,24 +27,33 @@ def registrar():
         nombre = request.form["nombres"]
         apellido = request.form["apellidos"]
         cargo = request.form["cargo"]
-        salario_base = request.form["salario_base"]
-        horas_extra = request.form["horas_extras"]
+        salario_base = float(request.form["salario_base"])
+        horas_extra = int(request.form["horas_extras"])
         tipo_hora_extra = request.form["tipo_hora_extra"]
-        horas_extras_adicionales = request.form["horas_extras_adicionales"]
-        tipo_hora_extra_adicionales = request.form["tipo_hora_extra_adicional"]
-        prestamo = request.form["prestamo"]
-        cuotas = request.form["cuotas"]
-        tasa_interese = request.form["tasa_interes_anual"]
+        horas_extras_adicionales = int(request.form["horas_extras_adicionales"])
+        tipo_hora_extra_adicional = request.form["tipo_hora_extra_adicional"]
+        prestamo = float(request.form["prestamo"])
+        cuotas = int(request.form["cuotas"])
+        tasa_interese = float(request.form["tasa_interes_anual"])
 
         nomina = Nomina(cedula_empleado = cedula, nombre_empleado = nombre, empleado_apellido = apellido, cargo = cargo,
                         salario_base = salario_base, horas_extras = horas_extra, tipo_hora_extra = tipo_hora_extra,
-                        horas_extras_adicionales = horas_extras_adicionales, tipo_hora_extra_adicional = tipo_hora_extra_adicionales,
+                        horas_extras_adicionales = horas_extras_adicionales, tipo_hora_extra_adicional = tipo_hora_extra_adicional,
                         prestamo = prestamo, cuotas = cuotas, tasa_interes = tasa_interese)
         return render_template("index.html")
 
 @app.route("/acceder", methods = ["GET", "POST"])
 def acceder():
-    ...
+    if request.method == "POST":
+        cedula = request.form["cedula"]
+        try:
+            usuario_buscado = NominaController.ObtenerEmpleadoPorCedula(cedula)
+            if usuario_buscado and usuario_buscado["cedula"] == cedula:
+                return render_template("panel.html")
+            
+        except Exception as e:
+                return render_template("index.html", mensaje = f"La cedula: {cedula} no se encuentra registrada")
+
 
 if __name__ == "__main__":
     app.run(debug = True)

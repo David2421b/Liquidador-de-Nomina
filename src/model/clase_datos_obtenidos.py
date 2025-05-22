7728589e6079f2989de68279c25adc5b538d1270
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-21 22:47:13 -0500

    se crea la clase para los datos

---
+++ src/model/clase_datos_obtenidos.py
@@ -0,0 +1,6 @@
+class DatosObtenidos:
+    def __init__(self, cedula: int, salario_neto, bonificacion, valor_hora_extra):
+        self.cedula = cedula
+        self.salario_neto = salario_neto
+        self.bonificacion = bonificacion
+        self.valor_hora_extra = valor_hora_extra

ddf38b55cd0b4ba5d45f769fa323fd0c3f2c2373
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-05-25 19:17:57 -0500

    correccion identacion

--- src/view/web/app.py
+++ src/view/web/app.py
@@ -7,7 +7,7 @@ from flask import render_template, request
 from src.view.web import plano
 
 app = Flask(__name__)
-app.register_blueprint( plano.blueprint)
+app.register_blueprint(plano.blueprint)
 
 
 if __name__ == "__main__":
